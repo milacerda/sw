1821d8a3c5f0cbc566f8afba7fd1ae2b46262595
Camila <milacerda@gmail.com>
2019-01-03 14:26:06 -0200

    passport

--- app/Planet.php
+++ app/Planet.php
@@ -4,6 +4,8 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 use Laravel\Passport\HasApiTokens;
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Foundation\Auth\User as Authenticatable;
 
 class Planet extends Model
 {
